55efdc61541d0ae32211e7b58ea3483e532a8cad
Ruisth23 <rmfde@iscte-iul.pt>
2023-05-14 22:02:29 +0100

    Feat: last and final version

---
+++ src/nn/NeuralEntity.java
@@ -0,0 +1,56 @@
+package nn;
+
+public class NeuralEntity implements Comparable<NeuralEntity> {
+    public double fitness;
+    public double[] node;
+
+    public NeuralEntity(){
+
+    }
+
+    public double getFitness() {
+        return fitness;
+    }
+
+    public void setFitness(double fitness) {
+        this.fitness = fitness;
+    }
+
+    @Override
+    public int compareTo(NeuralEntity other) {
+        return Double.compare(other.getFitness(), getFitness());
+    }
+
+
+    public double[] getNode() {
+        return node;
+    }
+
+    public void setNode(double[] node) {
+        this.node = node;
+    }
+
+    @Override
+    public String toString(){
+        int init = 0;
+        int hid = 0;
+        int out = 0;
+        double[][] inwei = null;
+        double[] hidbia = null;
+        double[] outbia = null;
+        double[][] outwei = null;
+        double fit = 0.0;
+        for (NeuralNetwork nn : GeneticAlgorithm.population) {
+            init = nn.getInputDim();
+            hid = nn.getHiddenDim();
+            out = nn.getOutputDim();
+            inwei = nn.getInputWeights();
+            hidbia = nn.getHiddenBiases();
+            outbia = nn.getOutputBiases();
+            outwei = nn.getOutputWeights();
+        }
+        fit = getFitness();
+
+        return "Neural Network: " + init + "- Input Dim | " + hid + "- Hidden Dim | " + out + "- Output Dim | " + inwei + "- Input Weights | " + hidbia + "- Hidden Biases | " + outbia + "- Output Biases | " + outwei + "- Output Weights | " + fit + "- Fitness | \n";
+    }
+}
--- src/nn/NeuralNetwork.java
+++ src/nn/NeuralNetwork.java
@@ -3,9 +3,6 @@ package nn;
 import controllers.GameController;
 import space.Board;
 
-import java.util.Arrays;
-import java.util.Random;
-
 public class NeuralNetwork implements GameController {
     private int inputDim;
     private int hiddenDim;
@@ -24,52 +21,49 @@ public class NeuralNetwork implements GameController {
         this.hiddenBiases = new double[hiddenDim];
         this.outputWeights = new double[hiddenDim][outputDim];
         this.outputBiases = new double[outputDim];
-        initializeWeights();
-    }
 
-    public NeuralNetwork(int inputDim, int hiddenDim, int outputDim, double[][] inputWeights, double[] hiddenBiases, double[][] outputWeights, double[] outputBiases) {
-        this.inputDim = inputDim;
-        this.hiddenDim = hiddenDim;
-        this.outputDim = outputDim;
-
-        this.inputWeights = inputWeights;
-        this.hiddenBiases = hiddenBiases;
-        this.outputWeights = outputWeights;
-        this.outputBiases = outputBiases;
     }
 
 
     public void initializeWeights(){
-        Random random = new Random();
 
         //Inicialização dos pesos de cada camada
-        inputWeights = new double[hiddenDim][inputDim];
-        for (int i = 0; i < hiddenDim; i++){
-            for (int j = 0; j < inputDim; j++){
-                inputWeights[i][j] = random.nextDouble() - 0.5;
+        inputWeights = new double[inputDim][hiddenDim];
+        for (int i = 0; i < inputDim; i++){
+            for (int j = 0; j < hiddenDim; j++){
+                inputWeights[i][j] = Math.random() - 0.5;
             }
         }
 
         //Inicialização das biases
         hiddenBiases = new double[hiddenDim];
         for (int i = 0; i < hiddenDim; i++){
-            hiddenBiases[i] = random.nextDouble() - 0.5;
+            hiddenBiases[i] = Math.random() - 0.5;
         }
 
         //Inicaliza os pesos da camada oculta para a camada de saída
-        outputWeights = new double[outputDim][hiddenDim];
-        for (int i = 0; i < outputDim; i++){
-            for (int j = 0; j < hiddenDim; j++){
-                outputWeights[i][j] = random.nextDouble() - 0.5;
+        outputWeights = new double[hiddenDim][outputDim];
+        for (int i = 0; i < hiddenDim; i++){
+            for (int j = 0; j < outputDim; j++){
+                outputWeights[i][j] = Math.random() - 0.5;
             }
         }
 
         //Inicialização das biases de saída
         outputBiases = new double[outputDim];
         for (int i = 0; i < outputDim; i++){
-            outputBiases[i] = random.nextDouble() - 0.5;
+            outputBiases[i] = Math.random() - 0.5;
         }
     }
+
+    public void setupBoard(){
+        Board b = new Board(this);
+        b.setSeed(5);
+        b.run();
+        fitness = b.getFitness();
+    }
+
+
 
     //Função sigmoide
     private double sigmoid(double x){
@@ -85,7 +79,8 @@ public class NeuralNetwork implements GameController {
         for(int i = 0; i < hiddenDim; i++){
             double activation = 0.0;
             for (int j = 0; j < inputDim; j++){
-                activation += inputWeights[i][j] * currentState[j];
+                double state = currentState[j];
+                activation += inputWeights[j][i] * state;
             }
             activation += hiddenBiases[i];
             hiddenActivations[i] = activation;
@@ -96,16 +91,16 @@ public class NeuralNetwork implements GameController {
         for (int i = 0; i < outputDim; i++){
             double activation = 0.0;
             for (int j = 0; j < hiddenDim; j++){
-                activation += outputWeights[i][j] * sigmoid(hiddenActivations[j]);
+                activation += hiddenActivations[j] * outputWeights[j][i];
             }
-            activation += outputBiases[i];
-            outputActivations[i] += activation;
+
+            outputActivations[i] = sigmoid(activation + outputBiases[i]);
         }
 
         //Aplicação da função de ativação final
         double[] output = new double[outputDim];
         for (int i = 0; i < outputDim; i++){
-            output[i] = outputActivations[i];
+            output[i] = sigmoid(outputActivations[i]);
         }
 
         return output;
@@ -171,16 +166,89 @@ public class NeuralNetwork implements GameController {
         return fitness;
     }
 
+    public int getTotalParams(){
+        return inputDim * hiddenDim + hiddenDim + hiddenDim * outputDim + outputDim;
+    }
+
+    public double[] getNode(){
+        double[] params = new double[getTotalParams()];
+
+        int index = 0;
+        for (int i = 0; i < inputDim; i++){
+            for (int j = 0; j < hiddenDim; j++){
+                params[index++] = inputWeights[j][i];
+            }
+        }
+
+        for (int i = 0; i < hiddenDim; i++){
+            params[index++] = hiddenBiases[i];
+        }
+
+        for (int i = 0; i < hiddenDim; i++) {
+            for (int j = 0; j < outputDim; j++){
+                params[index++] = outputWeights[i][j];
+            }
+        }
+
+        for (int i = 0; i < outputDim; i++){
+            params[index++] = outputBiases[i];
+        }
+
+        return params;
+    }
+
+    public void setNode(double[] params) {
+        if (params.length != getTotalParams()) {
+            throw new IllegalArgumentException("O tamanho do array de parâmetros não corresponde ao número esperado de parâmetros.");
+        }
+
+        int index = 0;
+        for (int i = 0; i < inputDim; i++) {
+            for (int j = 0; j < hiddenDim; j++) {
+                inputWeights[i][j] = params[index++];
+            }
+        }
+
+        for (int i = 0; i < hiddenDim; i++) {
+            hiddenBiases[i] = params[index++];
+        }
+
+        for (int i = 0; i < hiddenDim; i++) {
+            for (int j = 0; j < outputDim; j++) {
+                outputWeights[i][j] = params[index++];
+            }
+        }
+
+        for (int i = 0; i < outputDim; i++) {
+            outputBiases[i] = params[index++];
+        }
+    }
+
     public void setFitness(double fitness) {
         this.fitness = fitness;
     }
 
-    @Override
+   @Override
     public String toString() {
-        return "NeuralNetwork{" +
-                "inputDim=" + inputDim +
-                ", hiddenDim=" + hiddenDim +
-                ", outputDim=" + outputDim +
-                '}';
+        int init = 0;
+        int hid = 0;
+        int out = 0;
+        double[][] inwei = null;
+        double[] hidbia = null;
+        double[] outbia = null;
+        double[][] outwei = null;
+        double fit = 0.0;
+        for (NeuralNetwork nn : GeneticAlgorithm.population) {
+            init = nn.getInputDim();
+            hid = nn.getHiddenDim();
+            out = nn.getOutputDim();
+            inwei = nn.getInputWeights();
+            hidbia = nn.getHiddenBiases();
+            outbia = nn.getOutputBiases();
+            outwei = nn.getOutputWeights();
+            fit = nn.getFitness();
+        }
+
+        return "Neural Network: " + init + "- Input Dim | " + hid + "- Hidden Dim | " + out + "- Output Dim | " + inwei + "- Input Weights | " + hidbia + "- Hidden Biases | " + outbia + "- Output Biases | " + outwei + "- Output Weights | " + fit + "- Fitness | \n";
     }
 }
